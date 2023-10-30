523a0f99ced093a9053b03f7d094c400bf5534ee
andrei113322 <andrei.iutish@gmail.com>
2023-10-30 16:03:19 +0200

    Add project files.

---
+++ Model/User.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class User:BaseEntity
+    {
+        protected string userName;
+        public string UserName { get { return userName; } set { userName = value; } }
+
+        protected string firstName;
+        public string FirstName { get { return firstName; } set { firstName = value; } }
+
+        protected string secondName;
+        public string SecondName { get { return secondName; } set { secondName = value; } }
+
+        protected string password;
+        public string Password { get { return password; } set { password = value; } }
+
+        protected DateTime birthDate;
+        public DateTime BirthDate { get { return birthDate; } set { birthDate = value; } }
+
+        protected string email;
+        public string Email { get { return email; } set { email = value; } }
+
+        protected bool isAdmin;
+        public bool IsAdmin { get { return isAdmin; } set { isAdmin = value; } }
+    }
+
+    public class UserList : List<User>
+    {
+        //בנאי ברירת מחדל - אוסף ריק
+        public UserList() { }
+        //המרה אוסף גנרי לרשימת משתמשים
+        public UserList(IEnumerable<User> list)
+            : base(list) { }
+        //המרה מטה מטיפוס בסיס לרשימת משתמשים
+        public UserList(IEnumerable<BaseEntity> list)
+            : base(list.Cast<User>().ToList()) { }
+    }
+
+}
---
+++ ViewModel/UsersDB.cs
@@ -0,0 +1,49 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModel
+{
+    public class UsersDB : BaseDB
+    {
+        protected override BaseEntity NewEntity()
+        {
+            return new User();
+        }
+        protected override BaseEntity CreateModel(BaseEntity entity)
+        {
+            User user = entity as User;
+
+            user.ID = int.Parse(reader["ID"].ToString());
+            user.UserName = reader["USERNAME"].ToString();
+            user.FirstName = reader["FIRSTNAME"].ToString();
+            user.SecondName = reader["SECONDNAME"].ToString();
+            user.Password = reader["PASSWORD"].ToString();
+            user.BirthDate = DateTime.Parse(reader["BIRTHDATE"].ToString());
+            user.Email = reader["EMAIL"].ToString() ;
+            user.IsAdmin = Convert.ToBoolean(reader["ISADMIN"].ToString());
+
+            return user;
+        }
+
+        public UserList SelectAll()
+        {
+            command.CommandText = "SELECT * FROM USERS";
+            UserList list = new UserList(ExecuteCommand());
+            return list;
+        }
+
+        public User SelectById(int id)
+        {
+            command.CommandText = "SELECT * FROM USERS WHERE id=" + id;
+            UserList list = new UserList(ExecuteCommand());
+            if (list.Count == 0)
+                return null;
+            return list[0];
+        }
+
+    }
+}
