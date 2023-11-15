29b158f972d5bc7088771bbed58cabd346006055
andrei113322 <andrei.iutish@gmail.com>
2023-11-15 20:00:50 +0200

    Finished correcting the dataBase

--- Model/Log.cs
+++ Model/Log.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace Model
 {
-    internal class Log
+    public class Log:BaseEntity
     {
+        protected DateTime time;
+        public DateTime Time { get { return time; } set { time = value; } }
+
+        protected float pAndl;
+        public float PAndl { get { return pAndl; } set {pAndl = value; } }
+
+        protected string action;
+        public string Action { get { return action; } set { action = value; } } 
+    }
+
+    public class LogList : List<Log>
+    {
+        public LogList() { } //בנאי ברירת מחדל
+        public LogList(IEnumerable<Log> list) :
+            base(list)
+        { } //המרה של רשימת קורסים לאוסף של קורסים
+        public LogList(IEnumerable<BaseEntity> list) :
+            base(list.Cast<Log>().ToList())
+        { } //המרה כלפי מטה מישות בסיס לרשימת קורסים
+
     }
 }
--- ViewModel/OrderHistoryDB.cs
+++ ViewModel/OrderHistoryDB.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,42 @@ using System.Threading.Tasks;
 
 namespace ViewModel
 {
-    internal class OrderHistoryDB
+    internal class OrderHistoryDB : BaseDB
     {
+        protected override BaseEntity CreateModel(BaseEntity entity)
+        {
+            OrderHistory orderHistory = new OrderHistory();
+            orderHistory.Symbol = reader["SYMBOL"].ToString();
+            orderHistory.Side = reader["SIDE"].ToString();
+            orderHistory.Type = reader["TYPE"].ToString();
+            orderHistory.Qty = float.Parse(reader["QTY"].ToString());
+            orderHistory.Price = float.Parse(reader["PRICE"].ToString());
+            orderHistory.FillPrice = float.Parse(reader["FILLPRICE"].ToString());
+            orderHistory.Status = reader["STATUS"].ToString();
+            orderHistory.Placingtime = DateTime.Parse(reader["PLACINGTIME"].ToString());
+            orderHistory.ClosingTime = DateTime.Parse(reader["CLOSINGTIME"].ToString());
+
+            return orderHistory;
+            throw new NotImplementedException();
+        }
+
+        protected override BaseEntity NewEntity()
+        {
+            return new OrderHistory();
+        }
+
+        public OrderHistoryList SelectAll()
+        {
+            this.command.CommandText = "SELECT * FROM ORDERHISTORY";
+            OrderHistoryList list = new OrderHistoryList(base.ExecuteCommand());
+            return list;
+        }
+
+        public OrderHistoryList SelectByUser(User user)
+        {
+            this.command.CommandText = String.Format("SELECT * FROM ORDERHISTORY WHERE (USERID = {0})", user.ID);
+            OrderHistoryList list = new OrderHistoryList(base.ExecuteCommand());
+            return list;
+        }
     }
 }
