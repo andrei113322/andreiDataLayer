bf65241db645d73911bf19cd25a9b3b91ef0fe93
andrei113322 <andrei.iutish@gmail.com>
2023-12-17 19:08:07 +0200

    Solved some issues

---
+++ BinanceCCC/Connections.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Binance.API.Csharp.Client;
+
+namespace BinanceCCC
+{
+    public class Connections
+    {
+        private string apiKey = "CXX4LxqXPI8J8Q0jXaXcOduUGuaQ73P4V9xKD2jX62ly4zdpv2U0T5hqvg2i8evH";
+        private string SApiKey = "m7dDAp2tKvTvSeCI1jOeuVgORozwPpXsWTvHfCWAfxslydjDWg9imQGM1AB9sPMR";
+        private BinanceClient myBClient;
+
+        public Connections()
+        {
+            ApiClient myClient = new ApiClient(apiKey, SApiKey);
+            myBClient = new BinanceClient(myClient);
+        }
+
+        public decimal GetCriptoValue(string symbol)
+        {
+            try
+            {
+                var tickerPrices = myBClient.GetAllPrices().Result;
+                decimal result = Convert.ToDecimal(tickerPrices.FirstOrDefault(SymbolPrice => SymbolPrice.Symbol == symbol).Price);
+                return result;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception: {ex.Message}");
+                return -1; // or throw an exception, handle the error based on your requirements
+            }
+        }
+    }
+}
