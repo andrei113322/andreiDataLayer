b7eb30d634269412f7ddfa031b8a089a70f56feb
andrei113322 <andrei.iutish@gmail.com>
2024-04-11 11:15:34 +0300

    Made some updates

---
+++ Model/Admin.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    [DataContract]
+    public class Admin : BaseEntity
+    {
+        private double volume;
+        private int users;
+        private double assets;
+        private double liquidations;
+        private double profits;
+
+        [DataMember]
+        public double Volume { get { return volume; } set { volume = value; } }
+        [DataMember]
+        public int Users { get { return users; } set { users = value; } }
+        [DataMember]
+        public double Assets { get { return assets; } set { assets = value; } }
+        [DataMember]
+        public double Liquidations { get { return liquidations; } set {liquidations = value; } }
+        [DataMember]
+        public double Profits { get { return profits; } set { profits = value; } }
+    }
+
+    [CollectionDataContract]
+    public class AdminList : List<Admin>
+    {
+        //בנאי ברירת מחדל - אוסף ריק
+        public AdminList() { }
+        //המרה אוסף גנרי לרשימת משתמשים
+        public AdminList(IEnumerable<Admin> list)
+            : base(list) { }
+        //המרה מטה מטיפוס בסיס לרשימת משתמשים
+        public AdminList(IEnumerable<BaseEntity> list)
+            : base(list.Cast<Admin>().ToList()) { }
+    }
+}
---
+++ ViewModel/AdminDB.cs
@@ -0,0 +1,45 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModel
+{
+    public class AdminDB : BaseDB
+    {
+        protected override BaseEntity CreateModel(BaseEntity entity)
+        {
+            Admin myAdmin = new Admin();
+            myAdmin.Assets = double.Parse(reader["ASSETS"].ToString());
+            myAdmin.Profits = double.Parse(reader["PROFITS"].ToString());
+            myAdmin.Volume = double.Parse(reader["VOLUME"].ToString());
+            myAdmin.Users = int.Parse(reader["USERS"].ToString());
+            myAdmin.Liquidations = double.Parse(reader["LIQUIDATIONS"].ToString());
+            return myAdmin;
+        }
+
+        public AdminList getAdmin()
+        {
+            this.command.CommandText = "SELECT * FROM ADMIN";
+            AdminList list = new AdminList(base.ExecuteCommand());
+            return list;
+        }
+
+        public bool updateAdmin()
+        {
+            this.command.CommandText = $"";
+        }
+
+        protected override void LoadParameters(BaseEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override BaseEntity NewEntity()
+        {
+            return new Admin() as BaseEntity;
+        }
+    }
+}
